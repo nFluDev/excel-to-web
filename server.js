0d6207a1bd930f11a97167a9bfa919c91c101eb6
nFlu <nflu@nFlus-Mac-Pro.local>
2025-08-07 21:21:15 +0300

    update

--- server.js
+++ server.js
@@ -165,7 +165,7 @@ app.get('/:category', (req, res, next) => {
 
 // 404 Hata işleyici
 app.use((req, res, next) => {
-    res.status(404).send("404 Not Found: Aradığınız sayfa bulunamadı.");
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
 const server = app.listen(port, () => {
